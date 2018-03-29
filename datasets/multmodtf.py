f054dad43df0a68675b7b91ffd20d5f5421141e5
Nathan Piasco <nathan.piasco@gmail.com>
2018-03-29 11:29:19 +0200

    rename of tf for depth and RGB

--- datasets/multmodtf.py
+++ datasets/multmodtf.py
@@ -1,15 +1,15 @@
-from torchvision.transforms import RandomCrop as OriginalRandomCrop
-from torchvision.transforms import ToTensor as OriginalToTensor
-from torchvision.transforms import ColorJitter as OriginalColorJitter
-from torchvision.transforms import Resize as OriginalResize
-from torchvision.transforms import RandomResizedCrop as OriginalRandomResizedCrop
 import torchvision.transforms.functional as func
-from PIL import Image
+import PIL.Image
+import torchvision.transforms as tf
+import setlog
 
 
-class RandomCrop(OriginalRandomCrop):
+logger = setlog.get_logger(__name__)
+
+
+class RandomCrop(tf.RandomCrop):
     def __init__(self, size, padding=0):
-        OriginalRandomCrop.__init__(self, size=size, padding=padding)
+        tf.RandomCrop.__init__(self, size=size, padding=padding)
 
     def __call__(self, sample):
         img = sample['rgb']
@@ -25,23 +25,24 @@ class RandomCrop(OriginalRandomCrop):
         return sample
 
 
-class ToTensor(OriginalToTensor):
-    def __init__(self, error_value=65535, depth_factor=1e-3):
-        OriginalToTensor.__init__(self)
-        self.depth_factor = depth_factor
+class ToTensor(tf.ToTensor):
+    def __init__(self, error_value=65535):
+        tf.ToTensor.__init__(self)
         self.error_value = error_value
+        logger.info('Value error in the depth map set to {}'.format(error_value))
 
     def __call__(self, sample):
         sample['rgb'] = func.to_tensor(sample['rgb'])
         sample['depth'] = func.to_tensor(sample['depth']).float()
         sample['depth'][sample['depth'] == self.error_value] = 0
-        sample['depth'] *= self.depth_factor
         return sample
 
 
-class ColorJitter(OriginalColorJitter):
+class ColorJitter(tf.ColorJitter):
     def __init__(self, brightness=0.1, contrast=0.1, saturation=0.1, hue=0.05):
-        OriginalColorJitter.__init__(self, brightness=brightness, contrast=contrast, saturation=saturation, hue=hue)
+        tf.ColorJitter.__init__(self, brightness=brightness, contrast=contrast, saturation=saturation, hue=hue)
+        logger.info('Param for color jitter:')
+        logger.info('brightness={}, contrast={}, saturation={}, hue={}'.format(brightness, contrast, saturation, hue))
 
     def __call__(self, sample):
         transform = self.get_params(self.brightness, self.contrast,
@@ -50,9 +51,9 @@ class ColorJitter(OriginalColorJitter):
         return sample
 
 
-class Resize(OriginalResize):
+class Resize(tf.Resize):
     def __init__(self, size):
-        OriginalResize.__init__(self, size)
+        tf.Resize.__init__(self, size)
 
     def __call__(self, sample):
         sample['rgb'] = func.resize(sample['rgb'], self.size, self.interpolation)
@@ -60,9 +61,9 @@ class Resize(OriginalResize):
         return sample
 
 
-class RandomResizedCrop(OriginalRandomResizedCrop):
-    def __init__(self, size, scale=(0.25, 1.0), ratio=(3. / 4., 4. / 3.), interpolation=Image.BILINEAR):
-        OriginalRandomResizedCrop.__init__(self, size=size, scale=scale, ratio=ratio, interpolation=interpolation)
+class RandomResizedCrop(tf.RandomResizedCrop):
+    def __init__(self, size, scale=(0.25, 1.0), ratio=(3. / 4., 4. / 3.), interpolation=PIL.Image.BILINEAR):
+        tf.RandomResizedCrop.__init__(self, size=size, scale=scale, ratio=ratio, interpolation=interpolation)
 
     def __call__(self, sample):
         img = sample['rgb']
