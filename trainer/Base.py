2c98a99e928b301060ed124a3a288f6b2dea8951
Nathan Piasco <nathan.piasco@gmail.com>
2018-04-05 10:18:26 +0200

    removed unnecessary arguments

--- trainer/Base.py
+++ trainer/Base.py
@@ -7,12 +7,9 @@ logger = setlog.get_logger(__name__)
 
 class BaseTrainer:
     def __init__(self, **kwargs):
-        self.batch_size = kwargs.pop('batch_size', 25)
-        self.max_epoch = kwargs.pop('max_epoch', 100)
         self.lr = kwargs.pop('lr', 0.005)
         self.momentum = kwargs.pop('momentum', 0.9)
         self.weight_decay = kwargs.pop('weight_decay', 0.001)
-        self.shuffle = kwargs.pop('shuffle', True)
         self.cuda_on = kwargs.pop('cuda_on', True)
         self.optimizer_type = kwargs.pop('optimizer_type', 'SGD')
         if kwargs:
