9f0cbb64413e74277dd0d2984469462adf11a7fc
Nathan Piasco <nathan.piasco@gmail.com>
2018-04-12 17:02:26 +0200

    .cuda() at the right emplacement

--- trainer/minning_fonction.py
+++ trainer/minning_fonction.py
@@ -7,13 +7,13 @@ logger = setlog.get_logger(__name__)
 
 
 def default(trainer, batch, mode):
-    return trainer.network(trainer.cuda_func(auto.Variable(batch[mode][trainer.mod], requires_grad=True)))
+    return trainer.network(auto.Variable(trainer.cuda_func(batch[mode][trainer.mod]), requires_grad=True))
 
 
 def random(trainer, batch, mode):
     n = len(batch[mode])
     pick = rd.randint(0, n-1)
-    return trainer.network(trainer.cuda_func(auto.Variable(batch[mode][pick][trainer.mod], requires_grad=True)))
+    return trainer.network(auto.Variable(trainer.cuda_func(batch[mode][pick][trainer.mod]), requires_grad=True))
 
 def hard_minning(trainer, batch, mode):
     raise NotImplementedError
