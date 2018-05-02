e228793622a7bd833e6dd172c674b991bc003710
Nathan Piasco <nathan.piasco@gmail.com>
2018-05-02 10:31:24 +0200

    hard minning working

--- trainers/minning_function.py
+++ trainers/minning_function.py
@@ -1,6 +1,9 @@
 import setlog
 import torch.autograd as auto
 import random as rd
+import torch.nn.functional as func
+import numpy as np
+import torch
 
 
 logger = setlog.get_logger(__name__)
@@ -17,7 +20,23 @@ def random(trainer, batch, mode):
 
 
 def hard_minning(trainer, batch, mode):
-    raise NotImplementedError
+    anchors = trainer.network(auto.Variable(trainer.cuda_func(batch['query'][trainer.mod]),
+                                            requires_grad=False))
+    desc_anchors = anchors['desc']
+    exemples = torch.FloatTensor(batch['query'][trainer.mod].size())
+
+    for i, desc_anchor in enumerate(desc_anchors):
+        ex_descs = [trainer.network(auto.Variable(trainer.cuda_func(ex[trainer.mod][i:i+1]),
+                                                  requires_grad=False))['desc']
+                    for ex in batch[mode]]
+        diff = [func.pairwise_distance(desc_anchor.unsqueeze(0), x).data.cpu().numpy()[0, 0] for x in ex_descs]
+        sort_index = np.argsort(diff)
+        if mode == 'positives':
+            exemples[i] = batch[mode][sort_index[-1]][trainer.mod][i]
+        elif mode == 'negatives':
+            exemples[i] = batch[mode][sort_index[0]][trainer.mod][i]
+
+    return trainer.network(auto.Variable(trainer.cuda_func(exemples), requires_grad=True))
 
 
 def no_selection(trainer, batch, mode):
@@ -25,7 +44,6 @@ def no_selection(trainer, batch, mode):
         'desc': list(),
         'feat': list()
     }
-
     for ex in batch[mode]:
         forward = trainer.network(auto.Variable(trainer.cuda_func(ex[trainer.mod]), requires_grad=True))
         exemples['desc'].append(forward['desc'])
