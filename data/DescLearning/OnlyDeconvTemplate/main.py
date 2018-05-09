90139226c5a99fa77ad19d19dcb8a137790701aa
Nathan Piasco <nathan.piasco@gmail.com>
2018-05-09 17:25:17 +0200

    code for only deconv training (to have a good pretrained network)

---
+++ data/DescLearning/OnlyDeconvTemplate/main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import os, sys
+import setlog
+
+conf_file = os.environ['DEV'] + 'dl_management/.log/logging.yaml'
+save_file = os.path.abspath(sys.argv[0])[:-len(sys.argv[0])] + 'log/'
+setlog.reconfigure(conf_file, save_file)
+
+
+import system.DescriptorLearning as System
+
+
+if __name__ == '__main__':
+    machine = System.Deconv(root=os.path.abspath(sys.argv[0])[:-len(sys.argv[0])])
+    action = input("""Exec:
+    [t]\ttrain
+    [e]\ttest
+    [p]\tprint (console)
+    [P]\tprint (full)
+    [m]\tsee maps
+    [s]\tserialize net
+    [ ]\ttrain+test\n""")
+    if action == 't':
+        machine.train()
+    elif action == 'e':
+        machine.test()
+    elif action == 'p':
+        machine.plot(print_loss=False, print_val=False)
+    elif action == 'P':
+        machine.plot()
+    elif action == 'm':
+        machine.map_print()
+    elif action == 'm':
+        machine.serialize_net()
+    elif action == '':
+        machine.train()
+        machine.test()
+    else:
+        raise ValueError('Unknown cmd: {}'.format(action))
