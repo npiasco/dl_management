429cc3af85cbf4ef0d5ff6005bdca847c304d824
Nathan Piasco <nathan.piasco@gmail.com>
2018-06-04 11:23:14 +0200

    idx in the sample params

--- datasets/Platinum.py
+++ datasets/Platinum.py
@@ -96,6 +96,7 @@ class Platinum(utils.data.Dataset):
                     sample[mod] = torchvis.transforms.Compose(self.transform[mod])({mod: sample[mod]})[mod]
 
         sample['coord'] = self.data.ix[fidx, 1:3].as_matrix().astype('float')
+        sample['idx'] = fidx
         return sample
 
     @staticmethod
@@ -107,7 +108,7 @@ def show_batch(sample_batched):
     """Show image with landmarks for a batch of samples."""
     buffer = tuple()
     for name, mod in sample_batched.items():
-        if name not in ('coord',):
+        if name not in ('coord','idx'):
             min_v = mod.min()
             mod -= min_v
             max_v = mod.max()
@@ -125,7 +126,7 @@ if __name__ == '__main__':
 
     modtouse = ['rgb', 'depth', 'sem']
     transform = {
-        'first': (tf.Resize((256)),tf.RandomCrop(224)),
+        'first': (tf.Resize((230)),tf.RandomCrop(224)),
         'rgb': (tf.ToTensor(), ),
         'depth': (tf.ToTensor(),),
         'sem': (tf.ToTensor(),)
@@ -134,7 +135,8 @@ if __name__ == '__main__':
     dataset = Platinum(root=root_to_folders,
                        file='test_graph.csv',
                        modalities=modtouse,
-                       transform=transform)
+                       transform=transform,
+                       panorama_split=None)
 
     dataloader = utils.data.DataLoader(dataset, batch_size=6, shuffle=True, num_workers=2)
 
