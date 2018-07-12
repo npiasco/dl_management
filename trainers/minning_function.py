db6ae2434fbbae300f5fa0b06fdfa8c8a26bfc49
Nathan Piasco <nathan.piasco@gmail.com>
2018-07-12 16:29:43 +0200

    correction for mutnet trainers

--- trainers/minning_function.py
+++ trainers/minning_function.py
@@ -20,39 +20,56 @@ def static_vars(**kwargs):
     return decorate
 
 
-def default(trainer, batch, mode):
-    return trainer.network(auto.Variable(trainer.cuda_func(batch[mode][trainer.mod]), requires_grad=True))
+def default(network, batch, mode, **kwargs):
+    cuda_func = kwargs.pop('cuda_func', None)
+    mod = kwargs.pop('mod', None)
+    return_idx = kwargs.pop('return_idx', False)
+
+    if kwargs:
+        raise TypeError('Unexpected **kwargs: %r' % kwargs)
+
+    return network(auto.Variable(cuda_func(batch[mode][mod]), requires_grad=True))
 
 
-def random(trainer, batch, mode, return_idx=False):
+def random(network, batch, mode, **kwargs):
+    cuda_func = kwargs.pop('cuda_func', None)
+    mod = kwargs.pop('mod', None)
+    return_idx = kwargs.pop('return_idx', False)
+
+    if kwargs:
+        raise TypeError('Unexpected **kwargs: %r' % kwargs)
+
     n = len(batch[mode])
     pick = rd.randint(0, n-1)
     if return_idx:
-        return trainer.network(auto.Variable(trainer.cuda_func(batch[mode][pick][trainer.mod]), requires_grad=True)),\
-        [pick for i in range(batch[mode][0][trainer.mod].size(0))]
+        return network(auto.Variable(cuda_func(batch[mode][pick][mod]), requires_grad=True)),\
+        [pick for i in range(batch[mode][0][mod].size(0))]
     else:
-        return trainer.network(auto.Variable(trainer.cuda_func(batch[mode][pick][trainer.mod]), requires_grad=True))
+        return network(auto.Variable(cuda_func(batch[mode][pick][mod]), requires_grad=True))
 
 
-def hard_minning(trainer, batch, mode, **kwargs):
+def hard_minning(network, batch, mode, **kwargs):
     return_idx = kwargs.pop('return_idx', False)
     n_ex = kwargs.pop('n_ex', {'positives': 2, 'negatives': 5})
+    cuda_func = kwargs.pop('cuda_func', None)
+    mod = kwargs.pop('mod', None)
+
     if kwargs:
         raise TypeError('Unexpected **kwargs: %r' % kwargs)
 
     n_ex = n_ex[mode]
 
-    trainer.network.eval()
-    anchors = trainer.network(auto.Variable(trainer.cuda_func(batch['query'][trainer.mod]),
-                                            requires_grad=False))
+    network.eval()
+    anchors = network(auto.Variable(cuda_func(batch['query'][mod]),
+                                    requires_grad=False))
     desc_anchors = anchors
-    size = batch['query'][trainer.mod].size()
+    size = batch['query'][mod].size()
     size = torch.Size((n_ex, size[0], size[1], size[2], size[3]))
     exemples = torch.FloatTensor(size)
     idxs = [list() for _ in range(n_ex)]
     for i, desc_anchor in enumerate(desc_anchors):
-        ex_descs = [trainer.network(auto.Variable(trainer.cuda_func(ex[trainer.mod][i:i+1]),
-                                                  requires_grad=False))
+        ex_descs = [network(auto.Variable(cuda_func(ex[mod][i:i+1]),
+                                          requires_grad=False))
                     for ex in batch[mode]]
         diff = [func.pairwise_distance(desc_anchor.unsqueeze(0), x).data.cpu().numpy()[0, 0] for x in ex_descs]
         sort_index = np.argsort(diff)
@@ -61,14 +78,14 @@ def hard_minning(trainer, batch, mode, **kwargs):
         elif mode == 'negatives':
             idx = sort_index[:n_ex]
         for j in range(n_ex):
-            exemples[j][i] = batch[mode][idx[j]][trainer.mod][i]
+            exemples[j][i] = batch[mode][idx[j]][mod][i]
             idxs[j].append(idx[j])
 
-    trainer.network.train()
+    network.train()
     # Forward
     forwarded_ex = None
     for ex in exemples:
-        forward = trainer.network(auto.Variable(trainer.cuda_func(ex), requires_grad=True))
+        forward = network(auto.Variable(cuda_func(ex), requires_grad=True))
         if forwarded_ex is None:
             forwarded_ex = dict()
             for name, val in forward.items():
