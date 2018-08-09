22af5efd5fcbfdddae54e1d204b88cafdaa2c497
Nathan Piasco <nathan.piasco@gmail.com>
2018-08-09 15:35:28 +0200

    bug need use self

--- system/BaseClass.py
+++ system/BaseClass.py
@@ -110,9 +110,9 @@ class Base:
                 if hasattr(self.trainer, 'optimizers'):
                     lr_scheduler = dict()
                     for name, t_scheduler in self.lr_scheduler.items():
-                        lr_scheduler[name] = eval(self.lr_scheduler['class'])(self.trainer.optimizers[name],
-                                                                              **self.lr_scheduler['param'],
-                                                                              last_epoch=last_epoch)
+                        lr_scheduler[name] = eval(t_scheduler['class'])(self.trainer.optimizers[name],
+                                                                        **self.lr_scheduler['param'],
+                                                                        last_epoch=last_epoch)
 
                 else:
                     lr_scheduler = eval(self.lr_scheduler['class'])(self.trainer.optimizer,
