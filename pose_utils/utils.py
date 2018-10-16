dcc4eb261797b56ea1a34c3838d5c2a9d36878cb
Nathan Piasco <nathan.piasco@gmail.com>
2018-10-16 19:04:15 +0200

    file management

--- pose_utils/utils.py
+++ pose_utils/utils.py
@@ -2,6 +2,7 @@ import setlog
 import torch
 import matplotlib.pyplot as plt
 
+
 logger = setlog.get_logger(__name__)
 
 
@@ -25,21 +26,6 @@ def mat_proj(T, bVec, homo=False):
 
 def depth_map_to_pc(depth_map, K, remove_zeros=False):
     p = [[[i, j, 1] for j in range(depth_map.size(1))] for i in range(depth_map.size(2))]
-    p = torch.FloatTensor(p).transpose(0, 2).contiguous()
-
-    inv_K = K.inverse()
-    p_d = (p * depth_map).view(3, -1).transpose(0, 1)
-
-    if remove_zeros:
-        p_d = p_d[p_d.nonzero()][:, 0]
-    p_d = p_d.transpose(0, 1).contiguous()
-
-    x = mat_proj(inv_K, p_d)
-
-    return x
-
-def inv_depth_map_to_pc(inv_depth_map, K, remove_zeros=False):
-    p = [[[i, j, 1] for j in range(inv_depth_map.size(1))] for i in range(inv_depth_map.size(2))]
     p = torch.FloatTensor(p).transpose(0, 2).contiguous()
 
     inv_K = K.inverse()
@@ -83,3 +69,23 @@ def rotation_matrix(axis, theta):
     return torch.FloatTensor([[a*a+b*b-c*c-d*d, 2*(b*c-a*d), 2*(b*d+a*c)],
                               [2*(b*c+a*d), a*a+c*c-b*b-d*d, 2*(c*d-a*b)],
                               [2*(b*d-a*c), 2*(c*d+a*b), a*a+d*d-b*b-c*c]])
+
+
+def rot_to_quat(m):
+    if m[2, 2].item() < 0:
+        if m[0, 0].item() > m[1, 1].item():
+            t = 1 + m[0, 0] - m[1, 1] - m[2, 2]
+            q = torch.stack([m[1, 2] - m[2, 1], t, m[0, 1] + m[1, 0], m[2, 0] + m[0, 2]], 0)
+        else:
+            t = 1 - m[0, 0] + m[1, 1] - m[2, 2]
+            q = torch.stack([m[2, 0] - m[0, 2], m[0, 1] + m[1, 0], t, m[1, 2] + m[2, 1]], 0)
+    else:
+        if m[0, 0].item() < -m[1, 1].item():
+            t = 1 - m[0, 0] - m[1, 1] + m[2, 2]
+            q = torch.stack([m[0, 1] - m[1, 0], m[2, 0] + m[0, 2], m[1, 2] + m[2, 1], t], 0)
+        else:
+            t = 1 + m[0, 0] + m[1, 1] + m[2, 2]
+            q = torch.stack([t, m[1, 2] - m[2, 1], m[2, 0] - m[0, 2], m[0, 1] - m[1, 0]], 0)
+
+    q = q * 0.5 / torch.sqrt(t)
+    return q
