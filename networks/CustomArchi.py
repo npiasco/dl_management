74d75f365bae1e7a0b020a83e28ae81aa5ad2fc0
Nathan Piasco <nathan.piasco@gmail.com>
2018-10-18 18:36:19 +0200

    - can change the norm layer - big filter at the end for smooth map - TODO: variable output size

--- networks/CustomArchi.py
+++ networks/CustomArchi.py
@@ -4,6 +4,7 @@ import torch
 import networks.Descriptor as Desc
 import networks.ResNet as ResNet
 import networks.FeatAggregation as Agg
+import copy
 import collections as coll
 
 
@@ -74,39 +75,49 @@ class PixEncoder(nn.Module):
         i_channel = kwargs.pop('input_channels', 3)
         d_fact = kwargs.pop('d_fact', 1)
         k_size = kwargs.pop('k_size', 2)
+        norm_layer = kwargs.pop('norm_layer', 'group')
 
         if kwargs:
             raise TypeError('Unexpected **kwargs: %r' % kwargs)
+
+        if norm_layer == 'group':
+            norm_layer_func = lambda x: copy.deepcopy(nn.GroupNorm(x//2, x))
+        elif norm_layer == 'batch':
+            norm_layer_func = lambda x: copy.deepcopy(nn.BatchNorm2d(x))
+
 
         base_archi = [
             ('conv0', nn.Conv2d(i_channel, int(64 / d_fact), kernel_size=k_size, stride=2)),
             ('relu0', nn.LeakyReLU(0.2, inplace=True)),
             ('conv1', nn.Conv2d(int(64 / d_fact), int(128 / d_fact), kernel_size=k_size+1, stride=1, padding=1)),
-            ('bn1', nn.BatchNorm2d(int(128/d_fact))),
+            ('bn1', norm_layer_func(int(128/d_fact))),
             ('relu1', nn.LeakyReLU(0.2, inplace=True)),
             ('conv2', nn.Conv2d(int(128 / d_fact), int(256 / d_fact), kernel_size=k_size, stride=2)),
-            ('bn2', nn.BatchNorm2d(int(256 / d_fact))),
+            ('bn2', norm_layer_func(int(256 / d_fact))),
             ('relu2', nn.LeakyReLU(0.2, inplace=True)),
             ('conv3', nn.Conv2d(int(256 / d_fact), int(512 / d_fact), kernel_size=k_size+1, stride=1, padding=1)),
-            ('bn3', nn.BatchNorm2d(int(512 / d_fact))),
+            ('bn3', norm_layer_func(int(512 / d_fact))),
             ('relu3', nn.LeakyReLU(0.2, inplace=True)),
             ('conv4', nn.Conv2d(int(512 / d_fact), int(512 / d_fact), kernel_size=k_size, stride=2)),
-            ('bn4', nn.BatchNorm2d(int(512 / d_fact))),
+            ('bn4', norm_layer_func(int(512 / d_fact))),
             ('relu4', nn.LeakyReLU(0.2, inplace=True)),
             ('conv5', nn.Conv2d(int(512 / d_fact), int(512 / d_fact), kernel_size=k_size+1, stride=1, padding=1)),
-            ('bn5', nn.BatchNorm2d(int(512 / d_fact))),
+            ('bn5', norm_layer_func(int(512 / d_fact))),
             ('relu5', nn.LeakyReLU(0.2, inplace=True)),
             ('conv6', nn.Conv2d(int(512 / d_fact), int(512 / d_fact), kernel_size=k_size, stride=2)),
-            ('bn6', nn.BatchNorm2d(int(512 / d_fact))),
+            ('bn6', norm_layer_func(int(512 / d_fact))),
             ('relu6', nn.LeakyReLU(0.2, inplace=True)),
             ('conv7', nn.Conv2d(int(512 / d_fact), int(512 / d_fact), kernel_size=k_size+1, stride=1, padding=1)),
-            ('bn7', nn.BatchNorm2d(int(512 / d_fact))),
+            ('bn7', norm_layer_func(int(512 / d_fact))),
             ('relu7', nn.LeakyReLU(0.2, inplace=True)),
         ]
 
         self.feature = nn.Sequential(
             coll.OrderedDict(base_archi)
         )
+
+        logger.info('Final architecture is:')
+        logger.info(self.feature)
 
     def forward(self, x):
         output = dict()
@@ -131,36 +142,51 @@ class PixDecoder(nn.Module):
         d_fact = kwargs.pop('d_fact', 1)
         k_size = kwargs.pop('k_size', 2)
         out_pad = kwargs.pop('out_pad', 0)
+        div_fact = kwargs.pop('div_fact', 1)
+        norm_layer = kwargs.pop('norm_layer', 'group')
+
+        if kwargs:
+            raise TypeError('Unexpected **kwargs: %r' % kwargs)
+
+        if norm_layer == 'group':
+            norm_layer_func = lambda x: copy.deepcopy(nn.GroupNorm(x//2, x))
+        elif norm_layer == 'batch':
+            norm_layer_func = lambda x: copy.deepcopy(nn.BatchNorm2d(x))
+
         #TODO: construct various archi depending on the desired scale factor according to the output size
 
         base_archi = [
             ('conv7', nn.ConvTranspose2d(int(512 / d_fact), int(512 / d_fact), kernel_size=k_size+1, stride=1, padding=1)),
+            ('bn7', norm_layer_func(int(512 / d_fact))),
             ('relu7', nn.ReLU(inplace=True)),
-            ('conv6', nn.ConvTranspose2d(int(512 / d_fact * 2), int(512 / d_fact), kernel_size=k_size, stride=2, output_padding=out_pad)),
-            ('bn6', nn.BatchNorm2d(int(512 / d_fact))),
+            ('conv6', nn.ConvTranspose2d(int(512 / d_fact * 2), int(512 / d_fact), kernel_size=k_size, stride=2, output_padding=0)),
+            ('bn6', norm_layer_func(int(512 / d_fact))),
             ('relu6', nn.ReLU(inplace=True)),
             ('conv5', nn.ConvTranspose2d(int(512 / d_fact * 2), int(512 / d_fact), kernel_size=k_size+1, stride=1, padding=1)),
-            ('bn5', nn.BatchNorm2d(int(512 / d_fact))),
+            ('bn5', norm_layer_func(int(512 / d_fact))),
             ('relu5', nn.ReLU(inplace=True)),
-            ('conv4', nn.ConvTranspose2d(int(512 / d_fact * 2), int(512 / d_fact), kernel_size=k_size, stride=2, output_padding=0)),
-            ('bn4', nn.BatchNorm2d(int(512 / d_fact))),
+            ('conv4', nn.ConvTranspose2d(int(512 / d_fact * 2), int(512 / d_fact), kernel_size=k_size, stride=2, output_padding=out_pad)),
+            ('bn4', norm_layer_func(int(512 / d_fact))),
             ('relu4', nn.ReLU(inplace=True)),
             ('conv3', nn.ConvTranspose2d(int(512 / d_fact * 2), int(256 / d_fact), kernel_size=k_size+1, stride=1, padding=1)),
-            ('bn3', nn.BatchNorm2d(int(256 / d_fact))),
+            ('bn3', norm_layer_func(int(256 / d_fact))),
             ('relu3', nn.ReLU(inplace=True)),
-            ('conv2', nn.ConvTranspose2d(int(256 / d_fact * 2), int(128 / d_fact), kernel_size=k_size, stride=2, output_padding=0)),
-            ('bn2', nn.BatchNorm2d(int(128 / d_fact))),
+            ('conv2', nn.ConvTranspose2d(int(256 / d_fact * 2), int(128 / d_fact), kernel_size=k_size, stride=2, output_padding=out_pad)),
+            ('bn2', norm_layer_func(int(128 / d_fact))),
             ('relu2', nn.ReLU(inplace=True)),
             ('conv1', nn.ConvTranspose2d(int(128 / d_fact * 2), int(64 / d_fact), kernel_size=k_size+1, stride=1, padding=1)),
-            ('bn1', nn.BatchNorm2d(int(64 / d_fact))),
+            ('bn1', norm_layer_func(int(64 / d_fact))),
             ('relu1', nn.ReLU(inplace=True)),
-            ('conv0', nn.ConvTranspose2d(int(64 / d_fact * 2), int(out_channel), kernel_size=k_size, stride=2)),
+            ('conv0', nn.ConvTranspose2d(int(64 / d_fact * 2), int(out_channel), kernel_size=k_size*2, stride=2, padding=k_size//2)),
             ('sig0', nn.Sigmoid()),
         ]
 
         self.feature = nn.Sequential(
             coll.OrderedDict(base_archi)
         )
+
+        logger.info('Final architecture is:')
+        logger.info(self.feature)
 
     def forward(self, unet):
         for name, lay in self.feature.named_children():
@@ -181,8 +207,8 @@ class PixDecoder(nn.Module):
 
 
 if __name__ == '__main__':
-    input_size = 224
-    tensor_input = torch.rand([10, 3, input_size, input_size])
+    input_size = 224//2
+    tensor_input = torch.rand([1, 3, input_size, input_size])
     '''
     net = DeploymentNet()
 
@@ -197,8 +223,8 @@ if __name__ == '__main__':
 
     torch.save(net.state_dict(), 'default.pth')
     '''
-    enc = PixEncoder(k_size=2, d_fact=4)
-    dec= PixDecoder(k_size=2, d_fact=4, out_channel=1)
+    enc = PixEncoder(k_size=4, d_fact=4)
+    dec= PixDecoder(k_size=4, d_fact=4, out_channel=1, out_pad=1)
 
     feat_output = enc(tensor_input)
     output = dec(feat_output)
