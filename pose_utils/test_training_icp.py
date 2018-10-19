c577bd5ce2663e47732147c7d9653a3fd5f8b72b
Nathan Piasco <nathan.piasco@gmail.com>
2018-10-19 20:28:36 +0200

    model creation

--- pose_utils/test_training_icp.py
+++ pose_utils/test_training_icp.py
@@ -16,6 +16,7 @@ import torch.optim as optim
 import tqdm
 import matplotlib.pyplot as plt
 import numpy as np
+import networks.CustomArchi as CA
 
 
 def init_net():
@@ -57,14 +58,17 @@ def variable_hook(grad):
 
 
 if __name__ == '__main__':
-    ids = ['frame-000001','frame-000011', 'frame-000021']
-    scale_net = 1 / 2
+    ids = ['frame-000019','frame-000119', 'frame-000219', 'frame-000319', 'frame-000419', 'frame-000519',
+           'frame-000619', 'frame-000719', 'frame-000819', 'frame-000919']
+    scale_net = 1 / 4
 
-    scale = 1/2.142857142857143
+    scale = 1/4.285714285714286 / 8
+
 
     K = torch.zeros(3, 3)
     K[0, 0] = 585
-    K[0, 2] = 320
+    #K[0, 2] = 320
+    K[0, 2] = 240
     K[1, 1] = 585
     K[1, 2] = 240
 
@@ -72,8 +76,8 @@ if __name__ == '__main__':
 
     K[2, 2] = 1
 
-    #root = '/media/nathan/Data/7_Scenes/heads/seq-01/'
-    root = '/Users/n.piasco/Documents/Dev/seven_scenes/heads/seq-01/'
+    root = '/media/nathan/Data/7_Scenes/heads/seq-01/'
+    #root = '/Users/n.piasco/Documents/Dev/seven_scenes/heads/seq-01/'
 
     ims = list()
     ims_nn = list()
@@ -91,7 +95,9 @@ if __name__ == '__main__':
         ims.append(
             func.normalize(
                 func.to_tensor(
-                    func.resize(PIL.Image.open(rgb_im), int(480 * scale))
+                    func.center_crop(
+                    func.resize(PIL.Image.open(rgb_im), int(480 * scale)),
+                    int(480 * scale))
                 ).float(),
             mean=(0.485, 0.456, 0.406),
             std=(0.229, 0.224, 0.225)
@@ -104,7 +110,9 @@ if __name__ == '__main__':
         )
 
         depth = func.to_tensor(
+                func.center_crop(
                 func.resize(PIL.Image.open(depth_im), int(480 * scale), interpolation=0),
+                int(480 * scale))
         ).float()
         depth[depth == 65535] = 0
         depth *= 1e-3
@@ -129,63 +137,73 @@ if __name__ == '__main__':
 
         pcs.append(utils.toSceneCoord(depth, pose, K, remove_zeros=True))
 
-    pc_ref = torch.cat((pcs[0], pcs[2]), 1)
-
     pc_ref = torch.cat((pcs[0], pcs[1], pcs[2]), 1)
+    pc_ref = torch.load('base_model.pth')
 
-    torch.save(pc_ref.detach(), 'base_model.pth')
+    pc_ref = torch.cat(pcs, 1)
+
+    torch.save(pc_ref.detach(), 'full_model.pth')
+
     fig = plt.figure(3)
     ax = fig.add_subplot(111, projection='3d')
-    pas = 500
 
+    pas = 1
+    print(pc_ref.size())
     utils.plt_pc(pc_ref, ax, pas, 'b')
 
     plt.show()
 
     pc_ref.requires_grad = False
+    #pc_ref = torch.load('base_model.pth')
     pose = poses[1][:3,:]
 
     im_fwd = ims[1].unsqueeze(0)
     #net = init_net()
-    net = small_init_net()
+    #net = small_init_net()
+    net = nn.Sequential(
+            CA.PixEncoder(k_size=4, d_fact=4),
+            CA.PixDecoder(k_size=4, d_fact=4, out_channel=1, div_fact=4)
+    )
     q = utils.rot_to_quat(pose[:3, :3])
     t = pose[:3, 3]
 
-    optimizer = optim.Adam(net.parameters())
+    optimizer = optim.Adam(net.parameters(), lr=1e-4, weight_decay=1e-3)
     #net.register_backward_hook(module_hook)
     it = 10000
-    n_pt = 300
-    n_hyp = 5
+    n_hyp = 1
     tt_loss = list()
-    nb_pt_total = int(640 * 480 * scale**2)
+    #nb_pt_total = int(640 * 480 * scale**2)
+    nb_pt_total = int(480 * 480 * scale ** 2)
     param_icp = {
         'iter': 3,
         'fact': 2,
         'dnorm': False,
         'outlier': False
     }
+    n_pt = int(0.05 * nb_pt_total)
+
 
 
     init_pose = torch.eye(4,4)
     for i in tqdm.tqdm(range(it)):
         optimizer.zero_grad()
         inv_depth_map = net(im_fwd)
-        print(im_fwd.size())
-        print(inv_depth_map.size())
-
         depth_map = 1/inv_depth_map - 1
         Knet = K * scale_net
         Knet[-1,-1] = 1
+        print(im_fwd.size())
+        print(depth_map.size())
+        print(Knet)
         pc_to_align = utils.depth_map_to_pc(depth_map.squeeze(0), Knet)
 
         loss = 0
         for hyp in range(n_hyp):
             pc_to_align_pruned = pc_to_align.view(3,-1)[:, np.random.choice(int(nb_pt_total * scale_net**2), (n_pt), replace = False)]
-            pc_ref_pruned = pc_ref.view(3,-1)[:, np.random.choice(nb_pt_total, (n_pt), replace = False)]
-
-            #pose_net, dist = ICP.soft_icp(pc_ref_pruned, pc_to_align_pruned, init_T=init_pose, **param_icp)
-            pose_net, dist = ICP.soft_icp(pc_to_align_pruned, pc_ref_pruned, init_T=init_pose, **param_icp)
-            pose_net = pose_net.inverse()
+            #pc_ref_pruned = pc_ref.view(3,-1)[:, np.random.choice(nb_pt_total * 3, (n_pt)*3, replace = False)]
+            pc_ref_pruned = pc_ref
+            pose_net, dist = ICP.soft_icp(pc_ref_pruned, pc_to_align_pruned, init_T=init_pose, **param_icp)
+            #pose_net, dist = ICP.soft_icp(pc_to_align_pruned, pc_ref_pruned, init_T=init_pose, **param_icp)
+            #pose_net = pose_net.inverse()
 
             t_net = pose_net[:3, 3]
 
@@ -195,7 +213,7 @@ if __name__ == '__main__':
             q_loss = torch.mean(functional.pairwise_distance(q_net.view(-1, 1), q.view(-1, 1)))
             t_loss = torch.mean(functional.pairwise_distance(t_net.view(-1, 1), t.view(-1, 1)))
 
-            loss =+ 0.9*torch.max(torch.stack((q_loss, t_loss), 0)) + 0.1*torch.min(torch.stack((q_loss, t_loss), 0)) + 0.05*dist
+            loss =+ 0.9*torch.max(torch.stack((q_loss, t_loss), 0)) + 0.1*torch.min(torch.stack((q_loss, t_loss), 0))# + 0.05*dist
 
         #depth_map_loss = torch.mean(functional.pairwise_distance(depth_map.view(-1, 1), depths[1].view(-1, 1), p=1))
 
@@ -209,6 +227,10 @@ if __name__ == '__main__':
             print(t_net, q_net)
             print('Real pose is')
             print(t, q)
+
+            print(2*torch.acos(torch.dot(q_net, q)) * 180/3.14159260)
+            print(torch.sqrt(torch.sum((t_net - t)**2)))
+            print(torch.norm(t_net - t))
 
             fig = plt.figure(3)
             ax = fig.add_subplot(111, projection='3d')
