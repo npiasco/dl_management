5138b908873e36b4d671c112cc64b95b8c3ed100
Nathan Piasco <nathan.piasco@gmail.com>
2018-11-04 23:44:36 +0100

    added over algo for relative pc pose computation (not working that well)

--- networks/ICPNet.py
+++ networks/ICPNet.py
@@ -87,6 +87,108 @@ class CPNet(nn.Module):
 
         return T, utils.rot_to_quat(R), t
 
+    def directtf(self, pc1, pc2, indexor):
+        _, indices = torch.unique(pc2[0, :], return_inverse=True)
+        unique_indices = torch.unique(indices)
+        #M = pc1.new_zeros(indexor.nonzero().numel() * 2, 12)
+        A = pc1.new_zeros(indexor[unique_indices].nonzero().numel() * 3, 12)
+        B = pc1.new_zeros(indexor[unique_indices].nonzero().numel() * 3, 1)
+
+        if indexor.nonzero().numel() < 3:
+            logger.warn('Not enought correspondances to use dlt ({} match)'.format(
+                indexor.nonzero().numel()))
+        cpt = 0
+        for i in unique_indices:
+            if indexor[i].item():
+                A[cpt, :4] = pc1[:, i]
+                A[cpt + 1, 4:8] = pc1[:, i]
+                A[cpt + 2, 8:] = pc1[:, i]
+                B[cpt] = pc2[0, i]
+                B[cpt + 1] = pc2[1, i]
+                B[cpt + 2] = pc2[2, i]
+
+                cpt += 3
+
+        X, _ = torch.gels(B, A)
+        X = X[:12]
+
+        P = X.view(3, 4)
+        T = pc2.new_zeros(4, 4)
+        T[:3, :] = P
+        T[3, 3] = 1
+        #R = T[:3, :3]
+        #R = utils.quat_to_rot(utils.rot_to_quat(T[:3, :3]))
+
+        R = normalize_rotmat(T[:3, :3])
+        if torch.det(R) < 0:
+            print('Inverse')
+            R *= -1
+
+        T[:3, :3] = R
+
+        return T, utils.rot_to_quat(T[:3, :3]), T[:3, 3]
+
+    def dlt(self, pc1, pc2, indexor):
+
+        std, mean = torch.std(pc2[:3, :], 1), torch.mean(pc2[:3, :], 1)
+        T2std, T2mean = pc2.new_zeros(4, 4), pc2.new_zeros(4, 4)
+        T2mean[0, 0] = T2mean[1, 1] = T2mean[2, 2] = T2mean[3, 3] = 1
+        T2mean[:3, 3] = -mean
+        T2std[3, 3] = 1
+        for i in range(3):
+            T2std[i, i] = 1/std[i]
+        T2 = torch.matmul(T2std, T2mean)
+
+        pc2 = T2.matmul(pc2)
+
+        std, mean = torch.std(pc1[:3, :], 1), torch.mean(pc1[:3, :], 1)
+        T1std, T1mean = pc1.new_zeros(4, 4), pc1.new_zeros(4, 4)
+        T1mean[0, 0] = T1mean[1, 1] = T1mean[2, 2] = T1mean[3, 3] = 1
+        T1mean[:3, 3] = -mean
+        T1std[3, 3] = 1
+        for i in range(3):
+            T1std[i, i] = 1/std[i]
+        T1 = torch.matmul(T1std, T1mean)
+
+        pc1 = T1.matmul(pc1)
+
+        _, indices = torch.unique(pc2[0, :], return_inverse=True)
+        unique_indices = torch.unique(indices)
+        #M = pc1.new_zeros(indexor.nonzero().numel() * 2, 12)
+        M = pc1.new_zeros(indexor[unique_indices].nonzero().numel() * 2, 12)
+        print('Processing {} unique matches'.format(unique_indices.numel()))
+        if indexor[unique_indices].nonzero().numel() < 6:
+            logger.warn('Not enought correspondances to use dlt ({} match)'.format(indexor[unique_indices].nonzero().numel()))
+        cpt = 0
+        for i in unique_indices:
+            if indexor[i].item():
+                M[cpt, 4:] = torch.cat((-pc2[2, i]*pc1[:, i], pc2[1, i]*pc1[:, i]), 0)
+                M[cpt + 1, :4] = pc2[2, i]*pc1[:, i]
+                M[cpt + 1, 8:] = -pc2[0, i]*pc1[:, i]
+#                M[cpt + 2, :8] = torch.cat((-pt[1]*pc12[:, i], pt[0]*pc1[:, i]), 0)
+                cpt += 2
+
+        U, S, V = torch.svd(M)
+        p = V[:, -1]
+
+        if p[10].item() < 0:  # Diag of rot mat should be > 0
+            print('inverse')
+            p = p * -1
+
+        norm = (p[8] ** 2 + p[9] ** 2 + p[10] ** 2) ** 0.5
+        p = p / norm
+        P = p.view(3, 4)
+
+        # homogeneous transformation
+        T = pc2.new_zeros(4, 4)
+        T[:3, :] = P
+        T[3, 3] = 1
+        #T = T1.inverse().matmul(T.matmul(T2))
+        T = T2.inverse().matmul(T.matmul(T1))
+        print(T[:3, :3].matmul(T[:3, :3].t()))
+        T[:3, :3] = normalize_rotmat(T[:3, :3])
+        return T, utils.rot_to_quat(T[:3, :3]), T[:3, 3]
+
     def get_training_layers(self, layers_to_train=None):
         if layers_to_train is None:
             layers_to_train = self.layers_to_train
@@ -110,20 +212,43 @@ class CPNet(nn.Module):
         for i, pc in enumerate(pc1):
             pc_nearest, _ = self.soft_knn(pc, pc2[i])
             indexor = self.soft_outlier_rejection(pc, pc_nearest)
-            T[i], q[i], t[i] = self.soft_tf(pc, pc_nearest, indexor)
+            #T[i], q[i], t[i] = self.soft_tf(pc, pc_nearest, indexor)
+            T[i], q[i], t[i] = self.dlt(pc, pc_nearest, indexor)
+            #print(T[i])
+            #T[i], q[i], t[i] = self.directtf(pc, pc_nearest, indexor)
+            print(T[i])
 
         return {'T': T, 'q': q, 'p': t}
 
+def normalize_rotmat(R):
+    z = R[:, 2]/torch.norm(R[:, 2])
+    y = R[:, 1]
+    x = torch.cross(y, z)
+    x = x/torch.norm(x)
+    y = torch.cross(z, x)
+    R[:, 0] = x
+    R[:, 1] = y
+    R[:, 2] = z
+    return R
 
 if __name__ == '__main__':
-    net = CPNet()
-    p1 = torch.rand(2, 4, 5000)
+    net = CPNet(fact=2000)
+    p1 = torch.rand(1, 4, 40)
     p1[:, 3, :] = 1
-    p2 = p1 + 0.001
+    T = torch.zeros(4, 4)
+    T[:3, :3] = utils.rotation_matrix(torch.tensor([1.0, 0, 0]), torch.tensor([0.05]))
+    T[:3, 3] = torch.tensor([0.1, 0, 0])
+    T[3, 3] = 1
+    print(T)
+    p2 = T.matmul(p1)
+    """
+    p1 + 0.001
     p2[:, 3, :] = 1
-
+    """
     T = net(p1, p2)
-    print(T[0])
-    print(T[0].matmul(p1))
-    print(p2 - T[0].matmul(p1))
+    print(T['T'])
+    """
+    print(T['T'][0].matmul(p1))
+    print(p2 - T['T'][0].matmul(p1))
+    """
 
