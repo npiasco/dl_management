a976f14dee0a6df71804e6cab3fc6360e223c9a2
Nathan Piasco <nathan.piasco@gmail.com>
2018-12-06 10:28:48 +0100

    scripts updates

--- data/PoseReg/ICP/7scenes/Depth+Pose/main.py
+++ data/PoseReg/ICP/7scenes/Depth+Pose/main.py
@@ -2,6 +2,7 @@
 import os, sys
 import setlog
 
+
 conf_file = os.environ['DEV'] + 'dl_management/.log/logging.yaml'
 save_file = os.path.abspath(sys.argv[0])[:-len(sys.argv[0])] + 'log/'
 setlog.reconfigure(conf_file, save_file)
@@ -13,8 +14,9 @@ import system.PoseRegression as System
 if __name__ == '__main__':
     machine = System.MultNet(root=os.path.abspath(sys.argv[0])[:-len(sys.argv[0])],
                              #trainer_file='posenet_trainer.yaml',
-                             trainer_file='trainer.yaml',
+                             trainer_file='trainer_depth.yaml',
                              dataset_file='../datasets/heads.yaml'
+                             #dataset_file = '../datasets/minimal_heads.yaml'
                              )
     action = input('Exec:\n[t]\ttrain\n[e]\ttest\n[p]\tprint (console)\n[P]\tprint (full)\n[ ]\ttrain+test\n')
     if action == 't':
@@ -32,11 +34,19 @@ if __name__ == '__main__':
     elif action == 'm':
         machine.map_print(batch_size=1)
     elif action == 'mf':
-        machine.map_print(final=True, batch_size=1)
+        machine.map_print(final=True, batch_size=2)
     elif action == 'pose':
-        machine.view_localization(pas=10)
+        machine.view_localization(pas=3)
     elif action == 'posef':
-        machine.view_localization(pas=10, final=True)
+        machine.view_localization(pas=3, final=True)
+    elif action == 'model':
+        machine.creat_model()
+    elif action == 'modeld':
+        machine.creat_model(fake_depth=True)
+    elif action == 'modelt':
+        machine.creat_model(test=True)
+    elif action == 'modeldt':
+        machine.creat_model(test=True, fake_depth=True)
     elif action == '':
         machine.train()
         machine.test()
