18d3133224f150a824e5e0fe310cac37b150701d
Nathan Piasco <nathan.piasco@gmail.com>
2019-06-17 11:28:21 +0200

    script for HALL

---
+++ data/DescLearning/Journal/RGBtAux/H_A_VLAD/train_aux.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import os, sys
+import setlog
+
+
+conf_file = os.environ['DEV'] + 'dl_management/.log/logging.yaml'
+save_file = os.path.abspath(sys.argv[0])[:-len(sys.argv[0])] + 'log/'
+setlog.reconfigure(conf_file, save_file)
+
+
+import system.DescriptorLearning as System
+
+
+if __name__ == '__main__':
+    machine = System.MultNet(root=os.path.abspath(sys.argv[0])[:-len(sys.argv[0])],
+                             cnn_type='cnn.yaml',
+                             dataset_file='../../../SummerTests/datasets/cmu_lt.yaml',
+                             trainer_file='trainer_only_aux.yaml')
+
+    machine.train()
+    machine.serialize_net(final=True)
